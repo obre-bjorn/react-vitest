7b7883120a59e5570c1dd0fa4e6e77de4821c41f
obre-bjorn <obre.bjorn@gmail.com>
2024-03-08 18:09:52 +0300

    testing user interactions

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,25 @@
-import { describe, it, expect } from 'vitest';
+/* eslint-disable no-undef */
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-describe('something truthy and falsy', () => {
-  it('true to be true', () => {
-    expect(true).toBe(true);
+describe("App component", () => {
+  it("renders magnificent monkeys", () => {
+    // since screen does not have the container property, we'll destructure render to obtain a container for this test
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
   });
 
-  it('false to be false', () => {
-    expect(false).toBe(false);
+  it("renders radical rhinos after button click", async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+
+    screen.debug()
+    const button = screen.getByRole("button", { name: "Click Me" });
+
+    await user.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
   });
 });
